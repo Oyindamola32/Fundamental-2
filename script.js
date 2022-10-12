9a0aa3f451b044e29493c7508da94058e647cb10
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-12 23:01:13 +0100

    modify script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const yearsUntilRetirement = (birthYeah, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Jonas')); 
 console.log(yearsUntilRetirement(1980, 'Bob'));
-*/
+
 
 
 ///////////////////////////////////////
@@ -90,4 +90,27 @@ function cutFruitPieces(fruit) {
     return juice;
   }
   console.log(fruitProcessor(2, 3));
+  */
+
+ 
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
+  }
   
+  const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
+  
+    if (retirement > 0) {
+      console.log(`${firstName} retires in ${retirement} years`);
+      return retirement;
+    } else {
+      console.log(`${firstName} has already retired 🎉`);
+      return -1;
+    }
+  }
+  
+  console.log(yearsUntilRetirement(1991, 'Jonas'));
+  console.log(yearsUntilRetirement(1950, 'Mike'));
